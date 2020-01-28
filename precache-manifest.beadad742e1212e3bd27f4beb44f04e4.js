6e5dd7de56c738e456f7dd2759d4bf8293008375
James Abad <rjames1295@gmail.com>
2020-01-28 11:24:21 +0300

    Updates

---
+++ precache-manifest.beadad742e1212e3bd27f4beb44f04e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4957e592ca3c0121008425728cd13572",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "41235aacbb189a78747d",
+    "url": "/react-weather-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bc947332a5f43ed413cd",
+    "url": "/react-weather-app/static/js/2.f3173044.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-weather-app/static/js/2.f3173044.chunk.js.LICENSE"
+  },
+  {
+    "revision": "41235aacbb189a78747d",
+    "url": "/react-weather-app/static/js/main.d960f45d.chunk.js"
+  },
+  {
+    "revision": "bf86baad65e42e9424ce",
+    "url": "/react-weather-app/static/js/runtime-main.2092a7fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-weather-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
